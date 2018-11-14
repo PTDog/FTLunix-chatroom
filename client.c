83a1f3d6790a22dc6b3e1dc3e713d30334833ffc
PTDog <yanpan@andrew.cmu.edu>
2018-11-14 11:25:22 -0500

    added failover

--- client.c
+++ client.c
@@ -38,8 +38,9 @@ int main(int argc, char *argv[])
     char opt;
 
     char *buf,*msg_in; // send/receive buf and input buf
-    char hostname[MAXHOST]={"192.168.0.0"};
-    char chatname[MAXNAME]={"Client"};
+    char hostname[MAXHOST]="192.168.0.0";
+    char backup_host[MAXHOST]="192.168.0.0";
+    char chatname[MAXNAME]="Client";
 
     /* heartbeat relevant */
     int heartbeat_itv = 60; //default heatbeat interval (secs)
@@ -47,14 +48,16 @@ int main(int argc, char *argv[])
      * sending heartbeat, reset on received acknowledgement */
     int ack_request = 0;
     int verbose = 0;
+    int backup = 0; // backup existence flag, set if a backup server is assigned
 
     int port = 3490;
     strcpy(chatname,"Client");
-    while((opt = getopt(argc,argv,"hH:p:n:b:v"))!= -1){
+    while((opt = getopt(argc,argv,"hH:B:p:n:b:v"))!= -1){
         switch(opt){
             case 'h':
                 printf("\t-h\t\tprint this message\n");
                 printf("\t-H <host>\tdesignate server host\n");
+                printf("\t-B <host>\tassign backup server host.Default none.\n");
                 printf("\t-p <port>\tdesignate port number(3490 by default)\n");
                 printf("\t-n <chatname>\tEnter your chat name\n");
                 printf("\t-b <heartbeat>\tconfig heartbeat interval(60 secs by defalut)\n");
@@ -63,6 +66,10 @@ int main(int argc, char *argv[])
             case 'H':
                 strncpy(hostname,optarg,MAXHOST);
                 break;
+            case 'B':
+                backup = 1;
+                strncpy(backup_host,optarg,MAXHOST);
+                break;
             case 'p':
                 port = atoi(optarg);
                 break;
@@ -70,7 +77,8 @@ int main(int argc, char *argv[])
                 strncpy(chatname,optarg,MAXNAME);
                 break;
             case 'b':
-                heartbeat_itv = atoi(optarg);
+                // max hb 5 mins
+                heartbeat_itv = atoi(optarg) > 300 ? 300 :atoi(optarg);
                 break;
             case 'v':
                 verbose = 1;
@@ -118,7 +126,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 //    install sigalm handler for child process.
-    if (pid == 0){
+    if (pid){
         if(signal(SIGALRM,sigalrm_handler) == SIG_ERR){
             perror("Heartbeat initialization");
             exit(1);
@@ -126,8 +134,8 @@ int main(int argc, char *argv[])
         alarm(1); //send heartbeat almost immediately
     }
     while(1){
-        if(pid){
-        //父进程用于发送信息
+        if(!pid){
+        //子进程用于发送信息
 
 //        get_cur_time(time_str);
 
@@ -140,15 +148,15 @@ int main(int argc, char *argv[])
             //printf("---%s\n",buf);
             if((sendbytes = send(clientfd,buf,strlen(buf),0)) == -1){
                 perror("send\n");
-                //kill child
-                if(!kill(pid,SIGINT)){
+                //kill parent
+                if(!kill(getppid(),SIGINT)){
                     perror("Failed to terminate child process");
                 }
                 break;
             }
         }
         else{
-        //子进程用于接收信息以及收发心跳
+        //父进程用于接收信息以及收发心跳
             memset(buf,0,BUFSIZE);
             if(hb_ready == 1){
                 strcpy(buf,"Are you alive?");
@@ -161,7 +169,29 @@ int main(int argc, char *argv[])
                 if(ack_request == 2)
                     printf("Server unreachable, trying to reconnect...\n");
                 /* notify very 100 heartbeat interval */
-                ack_request = (ack_request < 100)? ack_request + 1 : 0;
+                ack_request = (ack_request < 5)? ack_request + 1 : ack_request;
+                /* timeout, bring backup */
+                if(backup && ack_request == 5){
+                    printf("Connect to backup server %s in  ",backup_host);
+                    for(int i = 5;i >= 0;i--){
+                        sleep(1);
+                        printf("\b%d",i);
+                        fflush(stdout);
+                    }
+                    printf("\n");
+                    if(kill(pid,SIGINT) == -1){ //kill parent
+                        perror("Failed to terminate parent process");
+                    }
+                    char _hb[4];
+                    snprintf(_hb,4,"%d",heartbeat_itv);
+                    char *args[60]={argv[0],"-H",backup_host,"-b",_hb,NULL};
+                    free(buf);
+                    free(msg_in);
+                    close(clientfd);
+                    if(execve(argv[0], argv, 0)<0)
+                        perror("Bringing backup");
+                    exit(0);
+                }
                 hb_ready = 0;
                 alarm(heartbeat_itv); //reset timer
             }
@@ -171,9 +201,9 @@ int main(int argc, char *argv[])
                     continue;
 
                 perror("recv");
-                //kill parent
-                if(!kill(getppid(),SIGINT)){
-                    perror("Failed to terminate parent process");
+                //kill child
+                if(kill(pid,SIGINT) == -1){
+                    printf("Failed to terminate parent process");
                 }
                 break;
             }
@@ -184,7 +214,11 @@ int main(int argc, char *argv[])
                 printf("%s\n",buf);
             }
             else
+            {
+                hb_ready = 0;
+                alarm(heartbeat_itv); //reset timer
                 printf("%s\n",buf);
+            }
         }
     }
     free(buf);
